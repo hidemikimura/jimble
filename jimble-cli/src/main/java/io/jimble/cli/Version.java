c26b78761a72e140671a71f5b4c338abde57932b
hidemikimura <hidemikimura@gmail.com>
2026-09-12 13:35:47 +0900

    no message

--- jimble-cli/src/main/java/io/jimble/cli/Version.java
+++ jimble-cli/src/main/java/io/jimble/cli/Version.java
@@ -12,7 +12,7 @@ package io.jimble.cli;
 final class Version {
 
 	/** マニフェストが無いとき（IDE から直接動かしたときなど） */
-	private static final String UNKNOWN = "0.6.0-SNAPSHOT";
+	private static final String UNKNOWN = "0.6.1-SNAPSHOT";
 
 	private Version () {}
 
